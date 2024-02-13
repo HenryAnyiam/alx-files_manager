0d19a3da792a5525637ae12f31b713bed29a870a
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-14 00:25:20 +0100

    db connect error

--- utils/db.js
+++ utils/db.js
@@ -4,15 +4,15 @@ const { MongoClient } = require('mongodb');
 class DBClient {
   
   constructor() {
-    let host = process.env.DB_HOST || 'localhost';
-    let port = process.env.DB_PORT || '27017';
-    let database = process.env.DB_DATABASE || 'files_manager';
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
     MongoClient.connect(url, (err, client) => {
       if (!err) {
-	this.db = client.db(database);
+        this.db = client.db(database);
       } else {
-	this.db = false;
+        this.db = false;
       }
     });
   }
