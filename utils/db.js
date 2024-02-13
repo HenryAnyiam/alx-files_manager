67144eab473c02a787b7e0ae48f59d4764d05950
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-13 16:17:34 +0100

    mongodb client

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  
+  constructor() {
+    let host = process.env.DB_HOST || 'localhost';
+    let port = process.env.DB_PORT || '27017';
+    let database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    MongoClient.connect(url, (err, client) => {
+      if (!err) {
+	this.db = client.db(database);
+      } else {
+	this.db = false;
+      }
+    });
+  }
+
+  isAlive() {
+    if (this.db) {
+      return true;
+    }
+    return false;
+  }
+
+  async nbUsers() {
+    let val = this.db.collection('users').countDocuments();
+    return val;
+  }
+
+  async nbFiles() {
+    let val = this.db.collection('files').countDocuments();
+    return val;
+  }
+
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
