2c10bf57e7371adb9de65c022bf577cf642986d6
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-14 00:28:40 +0100

    mount routes

---
+++ server.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+const express = require('express');
+import router  from './routes/index';
+
+const app = express();
+
+router(app);
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
